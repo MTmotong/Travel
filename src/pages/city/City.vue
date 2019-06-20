ad639c5c258dbdc69f3a6d18c9f8f21061234c47
dongziyu02 <dongziyu02@baidu.com>
2019-06-20 15:19:25 +0800

    add CityListPage Basic Layout

---
+++ src/pages/city/City.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <city-header></city-header>
+    <city-search></city-search>
+    <city-list></city-list>
+  </div>
+</template>
+
+<script>
+import CityHeader from './components/CityHeader'
+import CitySearch from './components/CitySearch'
+import CityList from './components/CityList'
+export default {
+  name: 'City',
+  components: {
+    CityHeader,
+    CitySearch,
+    CityList
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>

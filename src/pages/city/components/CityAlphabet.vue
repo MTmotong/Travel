e00cc7b76b498c14e70ede5cf2cceebf200f8772
dongziyu02 <dongziyu02@baidu.com>
2019-06-20 15:44:36 +0800

    add CityListPage Alphabet List

---
+++ src/pages/city/components/CityAlphabet.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="list">
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/variables.styl'
+  .list {
+    display flex
+    flex-direction column
+    justify-content center
+    position absolute
+    top 1.58rem
+    right 0
+    bottom 0
+    width .4rem
+    .item {
+      display flex
+      line-height .44rem
+      text-align center
+      color $bgColor
+    }
+  }
+</style>

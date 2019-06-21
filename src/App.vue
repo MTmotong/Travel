55e297033bcf7a7aa16651b516add8dec55c2e98
dongziyu02 <dongziyu02@baidu.com>
2019-06-21 18:03:58 +0800

    避免重复请求ajax的json数据,数据保活

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
-
-    <!-- 显示的当前路由地址所对应的内容 -->
-    <router-view/>
+    <keep-alive>
+      <!-- 显示的当前路由地址所对应的内容 -->
+      <router-view/>
+    </keep-alive>
   </div>
 </template>
 

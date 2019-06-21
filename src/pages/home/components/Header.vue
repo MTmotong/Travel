bc81daf9e0ce9228ebcb8aacea4236e3a24320ec
dongziyu02 <dongziyu02@baidu.com>
2019-06-21 16:56:34 +0800

    use Vuex && Vue Router && localStorage

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -17,10 +17,12 @@
 </template>
 
 <script>
+import { mapState, mapGetters } from 'vuex'
 export default {
   name: 'HomeHeader',
-  props: {
-    city: String
+  computed: {
+    ...mapState(['city']),
+    ...mapGetters(['doubleCity'])
   }
 }
 </script>
@@ -51,7 +53,8 @@ export default {
       padding-left .2rem
     }
     .header-right {
-      width: 1.24rem
+      min-width: 1.04rem
+      padding 0 .2rem
       float: right
       .arrow-icon {
         margin-left -.04rem
